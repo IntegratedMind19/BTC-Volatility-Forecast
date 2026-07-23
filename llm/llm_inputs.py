1e70a11515bd2328c48a98ce20c500a56221fca2
Nathan Chrisopherson Kwok <nathankwok.chrisopherson@gmail.com>
2026-07-23 11:00:44 +0700

    Create llm_inputs.py

---
+++ llm/llm_inputs.py
@@ -0,0 +1,17 @@
+import json
+
+def build_llm_inputs(prompt_template: str, knowledge_base: str, analysis_output: dict):
+  analysis_json = json.dumps(analysis_output, indent = 2)
+  return f'''
+  {prompt_template}
+
+  knowledge_base:
+  
+  {knowledge_base}
+
+  analysis_output:
+
+  {analysis_output}
+
+  Generate a complete report now.
+  '''
