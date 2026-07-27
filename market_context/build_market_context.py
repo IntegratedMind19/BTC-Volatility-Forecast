7388ba9a2869859fcf71276250ba6271bf1ba135
Nathan Chrisopherson Kwok <nathankwok.chrisopherson@gmail.com>
2026-07-27 13:18:49 +0700

    Update build_market_context.py

--- market_context/build_market_context.py
+++ market_context/build_market_context.py
@@ -2,11 +2,11 @@ from coingecko import retrieve_bitcoin_market_data
 from fred import retrieve_macro_data
 from news import retrieve_bitcoin_news
 from market_schema import empty_market_context
-from datetime import datetime
+from datetime import datetime, timezone
 
 def market_context_builder():
   market_context_dict = empty_market_context()
-  market_context_dict["retrieved_at"] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+  market_context_dict["retrieved_at"] = datetime.now(timezone.utc).strftime("%Y-%m-%d %H:%M:%S") + " (UTC)"
   market_context_dict["market_data"] = retrieve_bitcoin_market_data()
   market_context_dict["macro_data"] = retrieve_macro_data()
   
