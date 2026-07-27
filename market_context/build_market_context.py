27e86b55965b4406c551627e6227c2f38e2e79f7
Nathan Chrisopherson Kwok <nathankwok.chrisopherson@gmail.com>
2026-07-27 12:10:17 +0700

    Update build_market_context.py

--- market_context/build_market_context.py
+++ market_context/build_market_context.py
@@ -1,6 +1,17 @@
-import .coingecko
-from .fred import retrieve_macro_data
-from .news import retrieve_bitcoin_news
-import .market_schema
+import market_context
+from datetime import datetime
 
 def market_context_builder():
+  market_context_dict = market_context.empty_market_context()
+  market_context_dict["retrieved_at"] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+  market_context_dict["market_data"] = market_context.retrieve_bitcoin_market_data()
+  market_context_dict["macro_data"] = market_context.retrieve_macro_data()
+  
+  articles = market_context.retrieve_bitcoin_news()
+  market_context_dict["news"] = [article for article in articles if (article["summary"] and article["relevance_score"] > 0.2)][:5]
+  
+  market_context_dict["coverage"]["crypto_market"] = market_context_dict["market_data"]["status"]
+  market_context_dict["coverage"]["macroeconomics"] = ("available" if any(item.get("status") == "available" for item in market_context_dict["macro_data"].values()))
+  market_context_dict["coverage"]["news"] = ("available" if news)
+  
+  return market_context_dict
