2a9b1f9648f9c6e57d4c0bd1f0f9c85116401afd
Nathan Chrisopherson Kwok <nathankwok.chrisopherson@gmail.com>
2026-07-31 11:31:16 +0700

    Update create_report.py

--- llm/create_report.py
+++ llm/create_report.py
@@ -9,12 +9,21 @@ def create_report(
   knowledge_base_dir: str | Path, prompt_template_dir: str | Path, analysis_output: dict, market_context: dict, 
   client: OpenAI, model: str
 ) -> str:
-  knowledge_base = kb.load_knowledge_base(knowledge_base_dir)
-  prompt_template = pt.load_prompt_template(prompt_template_dir)
-  llm_input = llm_inputs.build_llm_inputs(prompt_template, knowledge_base, analysis_output, market_context)
-  response = ge.generate_explanation(client, llm_input, model)
-  return {
-    "analysis": analysis_output,
-    "market_context": market_context,
-    "report": response
-  }     
+  try:
+    knowledge_base = kb.load_knowledge_base(knowledge_base_dir)
+    prompt_template = pt.load_prompt_template(prompt_template_dir)
+    llm_input = llm_inputs.build_llm_inputs(prompt_template, knowledge_base, analysis_output, market_context)
+    response = ge.generate_explanation(client, llm_input, model)
+    return {
+      "status": "success",
+      "analysis": analysis_output,
+      "market_context": market_context,
+      "report": response
+    }
+  except Exception as exc:
+    return {
+      "status": "failed",
+      "analysis": None,
+      "market_context": None,
+      "report": None
+    }
