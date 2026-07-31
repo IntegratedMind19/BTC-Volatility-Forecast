dcddf61a2f0ea5221be1a9ed4a82dc7e06e822c7
Nathan Chrisopherson Kwok <nathankwok.chrisopherson@gmail.com>
2026-07-31 11:39:09 +0700

    Update generate_explanation.py

--- llm/generate_explanation.py
+++ llm/generate_explanation.py
@@ -1,10 +1,11 @@
 from openai import OpenAI
 
 def generate_explanation(client: OpenAI, llm_input: str, model: str):
-  response = client.responses.create(
-    model = model,
-    input = llm_input
-  )
-  if not response:
-    raise RuntimeError("Failed to generate proper response. Check whether some issues present.")
-  return response.output_text.strip()
+  try:
+    response = client.responses.create(
+      model = model,
+      input = llm_input
+    )
+    return response.output_text.strip()
+  except Exception as exc:
+    return "Failed to generate proper response. Check whether some issues present."
