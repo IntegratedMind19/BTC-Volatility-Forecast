5c3d9c178023954380d169ca8336f1869e16b5e7
Nathan Chrisopherson Kwok <nathankwok.chrisopherson@gmail.com>
2026-07-31 11:49:39 +0700

    Update build_market_context.py

--- llm/market_context/build_market_context.py
+++ llm/market_context/build_market_context.py
@@ -2,7 +2,7 @@ from llm.market_context.coingecko import retrieve_bitcoin_market_data
 from llm.market_context.fred import retrieve_macro_data
 from llm.market_context.news import retrieve_bitcoin_news
 from llm.market_context.market_schema import empty_market_context
-from llm.datetime import datetime, timezone
+from datetime import datetime, timezone
 
 def market_context_builder():
   market_context_dict = empty_market_context()
