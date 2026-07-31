0177eabe22c3876464d8dba849029fe687c41cff
Nathan Chrisopherson Kwok <nathankwok.chrisopherson@gmail.com>
2026-07-31 18:19:37 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, jsonify, render_template
 from llm.client import generate_report
+from llm.analysis_module import get_analysis_data, get_latest_vol_and_price
+from datetime import date, timedelta
 
 app = Flask(__name__, template_folder = "template")
 
@@ -7,14 +9,20 @@ app = Flask(__name__, template_folder = "template")
 def index():
   return render_template("index.html")
 
-@app.route("/api/forecast", methods = ["GET"])
+@app.get("/api/forecast")
 def forecast():
   result = generate_report()
   if result.get("status") != "success":
     return jsonify(result), 500
   return jsonify(result), 200
 
-@app.route("/forecast_explanation")
+@app.get("/api/chart_data")
+def get_chart_data():
+  chart_data = get_latest_vol_and_price()
+  chart_data["dates"] = [date.today() - date.timedelta(days = t) for t in range(29,-1,-1)]
+  return jsonify(chart_data)
+
+@app.route("/forecast")
 def forecast_explanation():
   render_template("forecast.html")
 
