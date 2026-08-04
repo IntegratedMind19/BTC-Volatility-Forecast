71d9e0c7df5c60933574cbe7e5c5a5e71b921be4
Nathan Chrisopherson Kwok <nathankwok.chrisopherson@gmail.com>
2026-08-04 17:09:48 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,25 +1,7 @@
-from pathlib import Path
-import os
-from dotenv import load_dotenv
 from flask import Flask, jsonify, render_template
 from llm.client import generate_report
 from llm.analysis_module import get_analysis_data, get_latest_vol_and_price
 from datetime import date, timedelta
-
-
-PROJECT_ROOT = Path(__file__).resolve().parent
-ENV_PATH = PROJECT_ROOT / ".env"
-
-loaded = load_dotenv(ENV_PATH, override=False)
-
-print(".env path:", ENV_PATH)
-print(".env exists:", ENV_PATH.is_file())
-print(".env loaded:", loaded)
-print("FRED key found:", bool(os.getenv("FRED_API_KEY")))
-print(
-    "Alpha Vantage key found:",
-    bool(os.getenv("ALPHA_VANTAGE_API_KEY"))
-)
 
 app = Flask(__name__, template_folder = "template")
 
