ab0d226c0b42a4610274790fe74ebbbe6e842666
Nathan Chrisopherson Kwok <nathankwok.chrisopherson@gmail.com>
2026-08-10 20:31:00 +0700

    Create forecast.js

---
+++ static/js/forecast.js
@@ -0,0 +1,88 @@
+const loadingScreen = document.getElementById("loading-screen");
+const errorScreen = document.getElementById("error-screen");
+const reportContent = document.getElementById("report-content");
+const errorMessage = document.getElementById("error-message")
+
+function showLoadingScreen(){
+  loadingScreen.hidden = false;
+  errorScreen.hidden = true;
+  reportContent.hidden = true;
+}
+
+function showErrorScreen(message){
+  loadingScreen.hidden = true;
+  errorScreen.hidden = false;
+  reportContent.hidden = true;
+  errorMessage.textContent = message;
+}
+
+function showReport(){
+  loadingScreen.hidden = true;
+  errorScreen.hidden = true;
+  reportContent.hidden = false;
+}
+
+async function retrieveForecastStatus(){
+  const response = await fetch("/api/forecast/status");
+  if(!response.ok){
+    throw new Error("Unable to get forecast status");
+  }
+  return response.json();
+}
+
+function pollUntilReady(){
+  const intervalId = setInterval(
+    async () => {
+      try {
+        status = await retrieveForecastStatus();
+        if(status.status === "error"){
+          showErrorScreen(status.error);
+          clearInterval(intervalId);
+        }
+        if(status.status === "ready"){
+          showReport();
+          clearInterval(intervalId);
+        }
+      }
+      catch(error) {
+        showErrorScreen(error.message);
+        clearInterval(intervalId);
+      }
+    }, 5000
+  )
+}
+
+async function loadLatestReport(){
+  const response = await fetch("/api/forecast/latest");
+  if(!response.ok){
+    throw new Error("Latest report is not available at the moment.");
+  }
+  const result = response.json();
+  if(result.status === "unavailable"){
+    throw new Error("Latest report is not available at the moment.");
+  }
+  renderReport();
+  showReport();
+}
+
+async function initializeForecastPage(){
+  try {
+    const status = await retrieveForecastStatus();
+    if(status.status === "updating") {
+      showLoadingScreen();
+      pollUntilReady();
+      return;
+    }
+    if(status.status === "ready") {
+      showReport();
+      return;
+    }
+    if(status.status === "error") {
+      showErrorScreen(status.error);
+      return;
+    }
+  }
+  catch(error) {
+    showErrorScreen(error.message);
+  }
+}
