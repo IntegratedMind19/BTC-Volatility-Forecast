a694d058e74011033f9dd208e205b871333a4caa
Nathan Chrisopherson Kwok <nathankwok.chrisopherson@gmail.com>
2026-08-11 13:55:15 +0700

    Update analysis_module.py

--- llm/analysis_module.py
+++ llm/analysis_module.py
@@ -268,4 +268,4 @@ def get_analysis_data():
         return None
 
 forecast_input = ForecastInputs()
-print(forecast_input.all_time_volatility.iloc[-29:].index)
+print(forecast_input.all_time_volatility.iloc[-29:].index.strftime('%d/%m/%Y').tolist())
