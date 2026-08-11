f50008e5c6475ad2531e9ab77d0223e6e35068cd
Nathan Chrisopherson Kwok <nathankwok.chrisopherson@gmail.com>
2026-08-11 12:35:44 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, jsonify, render_template
 from llm.client import generate_report
-from llm.analysis_module import get_analysis_data
 from datetime import date, timedelta, timezone, datetime
 from apscheduler.schedulers.background import BackgroundScheduler
 from scheduler import generate_scheduled_report
