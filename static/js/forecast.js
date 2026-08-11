982be5f8cb2fc201786a527deae2becf1b1e611e
Nathan Chrisopherson Kwok <nathankwok.chrisopherson@gmail.com>
2026-08-11 15:22:06 +0700

    Update forecast.js

--- static/js/forecast.js
+++ static/js/forecast.js
@@ -64,6 +64,7 @@ async function loadLatestReport(){
   if(result.status === "unavailable"){
     throw new Error("Latest report is not available at the moment.");
   }
+  console.log(result);
   document.getElementById("report-metadata").innerHTML = `<p> ${result.report} </p>`
   showReport();
 }
