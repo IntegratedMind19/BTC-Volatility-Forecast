2f64d854de29101e552d17edcaa2a5e267a8f1da
Nathan Chrisopherson Kwok <nathankwok.chrisopherson@gmail.com>
2026-08-11 12:39:04 +0700

    Update app.py

--- app.py
+++ app.py
@@ -50,8 +50,8 @@ def get_status():
 @app.get("/api/chart-data")
 def get_chart_data():
   report = load_report()
-  chart_data["price"] = report["historical_vol_and_price"]["price"]
-  chart_data["vol"] = report["historical_vol_and_price"]["vol"]
+  chart_data["price"] = report["analysis"]["historical_vol_and_price"]["price"]
+  chart_data["vol"] = report["analysis"]["historical_vol_and_price"]["vol"]
   chart_data["prediction"] = report["analysis"]["prediction"]["predicted_volatility"]
   chart_data["dates"] = [(datetime.now(timezone.utc).date() - timedelta(days = t)).strftime("%d/%m/%Y") for t in range(29,-1,-1)]
   chart_data["tomorrow_date"] = (datetime.now(timezone.utc).date() - timedelta(days = -1)).strftime("%d/%m/%Y")
