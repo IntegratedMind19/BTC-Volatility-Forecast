a07ff8e97eae90e9f6d6a04277b017261f36d56d
Nathan Chrisopherson Kwok <nathankwok.chrisopherson@gmail.com>
2026-08-11 13:17:35 +0700

    Update analysis_module.py

--- llm/analysis_module.py
+++ llm/analysis_module.py
@@ -257,8 +257,9 @@ def get_analysis_data():
                                           "volatility_regime_percentile": float(pred.vol_regime_percentile),
                                           "persistence_index": confidence.persistence,
                                           "ensemble_uncertainty_percentile": float(confidence.tree_disagreements_percentile)},
-                           "historical_vol_and_price": {'vol': forecast_input.all_time_volatility.iloc[-30:].tolist(),
-                                                  'price': forecast_input.btc["Close"].iloc[-29:].tolist()},
+                           "date_vol_and_price": {'vol': forecast_input.all_time_volatility.iloc[-30:].tolist(),
+                                                  'price': forecast_input.btc["Close"].iloc[-29:].tolist(),
+                                                 'date': forecast_input.btc["Close"].iloc[-29:].index.tolist()},
                            "metadata": {"timestamp": datetime.now(timezone.utc).strftime("%Y-%m-%d %H:%M:%S"),
                                         "model_version": "1.0"}
                            }
@@ -267,4 +268,4 @@ def get_analysis_data():
         return None
 
 forecast_input = ForecastInputs()
-print(forecast_input.btc.index)
+print(forecast_input.btc["Close"].iloc[-29:].index.tolist())
