3fa36d294ba1a8e5bcee3bd4e429d7a50e1b9039
Nathan Chrisopherson Kwok <nathankwok.chrisopherson@gmail.com>
2026-08-11 13:56:19 +0700

    Update analysis_module.py

--- llm/analysis_module.py
+++ llm/analysis_module.py
@@ -259,13 +259,11 @@ def get_analysis_data():
                                           "ensemble_uncertainty_percentile": float(confidence.tree_disagreements_percentile)},
                            "date_vol_and_price": {'vol': forecast_input.all_time_volatility.iloc[-30:].tolist(),
                                                   'price': forecast_input.btc["Close"].iloc[-29:].tolist(),
-                                                 'date': forecast_input.btc["Close"].iloc[-29:].index.strftime('%d/%m/%Y').tolist()},
+                                                 'date_price': forecast_input.btc["Close"].iloc[-29:].index.strftime('%d/%m/%Y').tolist(),
+                                                 'date_vol': forecast_input.all_time_volatility.iloc[-29:].index.strftime('%d/%m/%Y').tolist()}
                            "metadata": {"timestamp": datetime.now(timezone.utc).strftime("%Y-%m-%d %H:%M:%S"),
                                         "model_version": "1.0"}
                            }
         return analysis_output
     except Exception as exc:
         return None
-
-forecast_input = ForecastInputs()
-print(forecast_input.all_time_volatility.iloc[-29:].index.strftime('%d/%m/%Y').tolist())
