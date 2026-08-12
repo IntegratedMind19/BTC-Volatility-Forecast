760315ff5e5d95eceab2ad008d8e40c49ecf32aa
Nathan Chrisopherson Kwok <nathankwok.chrisopherson@gmail.com>
2026-08-12 20:44:24 +0700

    Update generate_explanation.py

--- llm/generate_explanation.py
+++ llm/generate_explanation.py
@@ -7,6 +7,6 @@ def generate_explanation(client: OpenAI, llm_input: str, model: str):
       model = model,
       input = llm_input
     )
-    return json.loads(response.output_text.strip())
+    return response.output_text.strip()
   except Exception as exc:
     return "Failed to generate proper response. Check whether some issues present."
