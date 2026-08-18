59968570703eb59b8bb7b88e36cd00d8fb74a6d8
Nathan Chrisopherson Kwok <nathankwok.chrisopherson@gmail.com>
2026-08-18 13:47:58 +0700

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from apscheduler.schedulers.background import BackgroundScheduler
 from scheduler import generate_scheduled_report
 from forecast_store import load_report, load_status
 
+'''
 scheduler = BackgroundScheduler(timezone = "UTC")
 
 scheduler.add_job(
@@ -18,6 +19,7 @@ scheduler.add_job(
   coalesce = True,
   max_instances = 1
 )
+'''
 
 if load_report() is None:
   generate_scheduled_report()
@@ -72,5 +74,5 @@ def about():
   return render_template("about.html")
 
 if __name__ == "__main__":
-  scheduler.start()
+  # scheduler.start()
   app.run(debug = True, use_reloader = False)
