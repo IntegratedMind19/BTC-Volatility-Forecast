417549f561b3ce31d470407dbd1376852cc13ca5
Nathan Chrisopherson Kwok <nathankwok.chrisopherson@gmail.com>
2026-08-20 17:06:11 +0700

    Update run_forecast.py

--- run_forecast.py
+++ run_forecast.py
@@ -1,4 +1,6 @@
+from forecast_store import initialize_store
 from scheduler import generate_scheduled_report
 
-if __name__ == "main":
+if __name__ == "__main__":
+    initialize_store()
     generate_scheduled_report()
